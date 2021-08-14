c1aeec366f44394320852a15c0a83b1b566f56ce
Meenu Singh <meenakshi15sept2000@gmail.com>
2021-08-14 14:45:16 +0530

    Co-authored-by: Sonalanand102 <Sonalanand102@users.noreply.github.com>

---
+++ db_config.php
@@ -0,0 +1,17 @@
+<?php
+$server = "localhost";
+    $username = "root";
+    $password = "";
+    $db = "hstl_mngmt";
+
+    $con = mysqli_connect($server, $username, $password, $db);
+
+    if (!$con){
+        die("<script>Connection failed due to </script>" . mysqli_connect_error());
+    }
+    else{
+        echo "<script>Successfull connection</script>";
+    }
+    session_start();
+    error_reporting(1);
+?>    
